6501f6537d5129cc411f27faa4738838c5522e57
nose <yellow4157@naver.com>
2021-01-07 00:14:12 +0900

    re

---
+++ Stage7/prob11720.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class prob11720 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] array = new int[n];
+		int sum = 0;
+		String a = br.readLine();
+		for(int i = 0; i < n; i++) {
+			array[i] = a.charAt(i) - '0';
+			sum += array[i];
+		}
+		
+		System.out.print(sum);
+	}
+}
