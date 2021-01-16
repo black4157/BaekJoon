af418ae927768621dd4289162b739d1b850c08a6
nose <yellow4157@naver.com>
2021-01-16 22:25:34 +0900

    prob2839

---
+++ Stage8/prob2839.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class prob2839 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		int three = 0;
+		int five = 0;
+		
+		while( n % 5 != 0 && n >= 0 ) {
+			n -= 3;
+			three++;
+		}
+		
+		if( n < 0 ) bw.write("-1");
+		else {
+			five = n / 5;
+			bw.write(String.valueOf(three + five));
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
